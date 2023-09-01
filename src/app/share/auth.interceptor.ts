c83dd76c0f15dfd9f4d334ee972fbf22dc818368
venureddy759 <evgr759@gmail.com>
2023-09-01 16:36:00 +0530

    refresh token added

--- src/app/share/auth.interceptor.ts
+++ src/app/share/auth.interceptor.ts
@@ -1,4 +1,3 @@
-// auth.interceptor.ts
 import { Injectable } from '@angular/core';
 import {
   HttpInterceptor,
@@ -6,12 +5,13 @@ import {
   HttpHandler,
   HttpEvent,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { switchMap, take } from 'rxjs/operators';
+import { switchMap, take, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  authService: any;
   constructor(private afAuth: AngularFireAuth) {}
 
   intercept(
@@ -19,17 +19,54 @@ export class AuthInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return this.afAuth.idToken.pipe(
-      take(1), // Take the current token and complete the observable
+      take(1),
       switchMap((idToken) => {
         if (idToken) {
-          request = request.clone({
-            setHeaders: {
-              Authorization: `Bearer ${idToken}`,
-            },
-          });
+          request = this.addAuthorizationHeader(request, idToken);
         }
-        return next.handle(request);
+        return next.handle(request).pipe(
+          catchError((error) => {
+            // Check if the error is due to an expired token
+            if (error.status === 401 && error.error?.message === 'TOKEN_EXPIRED') {
+              return this.refreshTokenAndRetry(request, next);
+            }
+            // If it's not an expired token error, propagate the error
+            throw error;
+          })
+        );
       })
     );
+  }
+
+  private addAuthorizationHeader(
+    request: HttpRequest<any>,
+    idToken: string
+  ): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    });
+  }
+
+  private refreshTokenAndRetry(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    
+    return this.authService.refreshToken().pipe(
+      switchMap((newToken:string) => {
+        if (newToken) {
+          const updatedRequest = this.addAuthorizationHeader(request, newToken);
+          return next.handle(updatedRequest);
+        } else {
+          // Handle token refresh failure (e.g., logout user)
+          this.authService.logout();
+          return EMPTY; // Return an empty observable or throw an error if needed
+        }
+      })
+    );
+  
+   
   }
 }
